ab52a8af55891c79f9c912176a86350757a527f3
Antoine ROY <antoineroy92@gmail.com>
2023-07-13 15:07:10 +0200

    Début du front avec react

--- backend/models/Projects.js
+++ backend/models/Projects.js
@@ -5,10 +5,7 @@ const mongoose = require('mongoose')
 
 const projectShema = mongoose.Schema({
     title: { type: String, required: true },
-    languagesId: [
-        {
-            languageId: String
-        }],
+    languagesId: [{ languageId: String }],
 })
 
 module.exports = mongoose.model('Project', projectShema);
