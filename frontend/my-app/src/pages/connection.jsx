576883d35e403a57c0a378fbfaabbf23902fe5a4
Antoine ROY <antoineroy92@gmail.com>
2023-07-25 11:56:33 +0200

    Regex Front

--- frontend/my-app/src/pages/connection.jsx
+++ frontend/my-app/src/pages/connection.jsx
@@ -6,76 +6,96 @@ export default function Connection() {
 
     function userSignUp(event) {
         event.preventDefault()
-        //on récupere les infos dans le but de les envoyer
-        let user = {
-            email: event.target.querySelector("[name=email]").value,
-            password: event.target.querySelector("[name=password]").value,
-            name: event.target.querySelector("[name=username]").value
-        };
-        console.log(user)
-        // Appel de la fonction fetch avec toutes les informations nécessaires
-        fetch("http://localhost:3000/api/auth/signup", {
-            method: "post",
-            headers: { "Content-Type": "application/json", "accept": "application/json" },
-            body: JSON.stringify(user)
-        })
-            //verifier qu'il y a une réponse et la renvoie en json
-            .then(function (response) {
-                if (response.ok) {
-                    return response.json();
-                }
-            })
-            //redirection vers la home page
-            // .then(document.location.href = "./")
-            // si l'authentification n'a pas fonctionné
-            .catch(function (error) {
-                console.log(error);
-                window.alert("Une erreur est survenue, veuillez réessayer")
-            });
+        let email = event.target.querySelector("[name=email]").value
+        let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
+        //Le nom d'utilisateur doit contenir uniquement des lettres minuscules et majuscules, des chiffres et éventuellement des tirets ou des underscores. Il ne peut pas commencer ou se terminer par un tiret ou un underscore, et ne peut pas en avoir deux consécutifs.
+        let name = event.target.querySelector("[name=username]").value
+        let nameRegex = /^[a-zA-Z0-9](?!.*--|__)[a-zA-Z0-9-_]+$/;
+        //Le mot de passe doit avoir une longueur minimale de caractères, par exemple, au moins 8 caractères & Il doit contenir au moins une lettre majuscule, une lettre minuscule et un chiffre.
+        let password = event.target.querySelector("[name=password]").value
+        let passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d!@#$%^&*]{8,}$/
+        if (emailRegex.test(email) && nameRegex.test(name) && passwordRegex.test(password)) {
+            console.log("testRegex")
+            let user = {
+                email: email,
+                password: password,
+                name: name
+            };
+            // Appel de la fonction fetch avec toutes les informations nécessaires
+            fetch("http://localhost:3000/api/auth/signup", {
+                method: "post",
+                headers: { "Content-Type": "application/json", "accept": "application/json" },
+                body: JSON.stringify(user)
+            })
+                //verifier qu'il y a une réponse et la renvoie en json
+                .then(function (response) {
+                    if (response.ok) {
+                        return response.json();
+                    }
+                })
+                .catch(function (error) {
+                    console.log(error);
+                    window.alert("Une erreur est survenue, veuillez réessayer");
+                });
+        } else if (emailRegex.test(email) === false) {
+            window.alert("Veuillez entrer une adresse mail valide");
+        } else if (nameRegex.test(name) === false) {
+            window.alert("Veuillez entrer un nom valide");
+        } else if (passwordRegex.test(password) === false) {
+            window.alert("Veuillez entrer un mot de passe contenant une lettre majuscule, une minuscule et un chiffre");
+        }
     }
 
 
     function userLogin(event) {
         event.preventDefault()
-        let user = {
-            email: event.target.querySelector("[name=email]").value,
-            password: event.target.querySelector("[name=password]").value,
-        };
-        // Création de la charge utile au format JSON
-        let chargeUtile = JSON.stringify(user);
-        console.log(chargeUtile)
-        // Appel de la fonction fetch avec toutes les informations nécessaires
-        fetch("http://localhost:3000/api/auth/login", {
-            method: "post",
-            headers: { "Content-Type": "application/json", "accept": "application/json" },
-            body: chargeUtile
-        })
-            //verifier qu'il y a une réponse et la renvoie en json
-            .then(function (response) {
-                if (response.ok) {
-                    setLogin(true);
-                    console.log(login)
-                    return response.json();
-                }
+        let email = event.target.querySelector("[name=email]").value
+        let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (emailRegex.test(email)) {
+            let user = {
+                email: event.target.querySelector("[name=email]").value,
+                password: event.target.querySelector("[name=password]").value,
+            }
+            // Création de la charge utile au format JSON
+            let chargeUtile = JSON.stringify(user);
+            console.log(chargeUtile)
+            // Appel de la fonction fetch avec toutes les informations nécessaires
+            fetch("http://localhost:3000/api/auth/login", {
+                method: "post",
+                headers: { "Content-Type": "application/json", "accept": "application/json" },
+                body: chargeUtile
             })
-            //traitement de la réponse
-            .then(function (contenu) {
-                //on nettoie le local storage
-                localStorage.clear();
-                // on met dans le local storage le Token et l'Id du user
-                localStorage.setItem("responseToken", contenu.token);
-                localStorage.setItem("responseId", contenu.userId);
-                //on récupère l'heure et la date et on les sauvegardes dans le localStorage
-                localStorage.setItem("DerniereConnexion", new Date());
-                // On envoie vers la page d'acueil
-                document.location.href = "./";
-            })
-            // si l'authentification n'a pas fonctionné
-            .catch(function (error) {
-                console.log(error);
-                window.alert("Veuillez entrez des id corrects")
-            });
+                //verifier qu'il y a une réponse et la renvoie en json
+                .then(function (response) {
+                    if (response.ok) {
+                        setLogin(true);
+                        console.log(login)
+                        return response.json();
+                    }
+                })
+                //traitement de la réponse
+                .then(function (contenu) {
+                    //on nettoie le local storage
+                    localStorage.clear();
+                    // on met dans le local storage le Token et l'Id du user
+                    localStorage.setItem("responseToken", contenu.token);
+                    localStorage.setItem("responseId", contenu.userId);
+                    //on récupère l'heure et la date et on les sauvegardes dans le localStorage
+                    localStorage.setItem("DerniereConnexion", new Date());
+                    // On envoie vers la page d'acueil
+                    document.location.href = "./";
+                })
+                // si l'authentification n'a pas fonctionné
+                .catch(function (error) {
+                    console.log(error);
+                    window.alert("Veuillez entrez des id corrects")
+                });
+        } else {
+            window.alert("Veuillez entrez une adresse mail valide")
+        }
+
+
     }
 
     return (
